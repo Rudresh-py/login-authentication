c6905aabe738a7a3704f730c41f5159d9846c1f8
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2022-09-28 19:47:37 +0530

    update

--- login/authentication/views.py
+++ login/authentication/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
-
-# Create your views here.
 def home(request):
     return render(request,"authentication/index.html")
 
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
-        fname = request.POST['fname']
-        lname = request.POST['lname']
-        email = request.POST['email']
-        pass1 = request.POST['pass1']
+        fname = request.POST['fname'],
+        lname = request.POST['lname'],
+        email = request.POST['email'],
+        pass1 = request.POST['pass1'],
         pass2 = request.POST['pass2']
 
         myuser = User.objects.create_user(username,email,pass1)
